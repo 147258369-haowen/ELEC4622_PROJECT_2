fdd326277d78dc7978bd3e35275b211bd4620b0b
DESKTOP-5K2KUC1\Haowen Jiang <haowen.jiang@uq.net.au>
2024-07-13 14:43:17 +1000

    task3 and 4

---
+++ Task4/Task4p2/Task4p2.cpp
@@ -0,0 +1,256 @@
+﻿// Task_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include "io_bmp.h"
+#include "filter.h"
+#include "image_comps.h"
+#include <math.h>
+extern float laplacianKernel[3][3];
+int main(int argc, char* argv[]) {
+    STATE state = CHECK_INPUT;
+    int filterChoose = 0;
+    bmp_in in;
+    bmp_out out;
+    my_image_comp* input_comps = nullptr;
+    my_image_comp* output_comps = nullptr;
+    io_byte* line = nullptr;
+    float** matrix = nullptr;
+    my_image_comp* temp_comps = nullptr;
+    my_image_comp* temp_comps_out = nullptr;
+    my_image_comp* overlaid = nullptr;
+    my_image_comp* nextinput = nullptr;
+    my_image_comp** lapalacin = nullptr;
+    my_image_comp* temp_image_2 = nullptr;
+    ImageParam imageParam;
+    float sigma = 0;
+    float** lapkernel = allocateMatrix(3);
+    Filter* sinc = nullptr;
+    int newheight = 0;
+    int tempheight = 0;
+    while (true) {
+        switch (state) {
+        case CHECK_INPUT:
+            CheckInput(argc, argv, &sigma, &filterChoose, &imageParam);
+            sinc = new Filter(imageParam.H, imageParam.D);
+            sinc->SincKernelGenerate(sinc->sinc_buffer);
+            imageParam.sincWidth = ((sinc->length) * 2 + 1);
+            printf("[");
+            for (int i = 0; i < imageParam.sincWidth; i++) {
+                printf("%f,", sinc->sinc_buffer[i]);
+            }
+            printf("]\n");
+            state = LOAD_PICTURE;
+            break;
+        case LOAD_PICTURE:
+            //printf("%f\r\n", FilterInit(matrix, HIGHT, WIDTH));//归一化
+            imageParam.GaussianDimension = GaussianWindowDimensionChoose(sigma);
+            LoadImage(&in, &input_comps, &output_comps, &line, &imageParam, &filterChoose, argv);
+            state = LAPLACIAM_PYRAMID;
+            break;
+        case GAUSSIAN_FILTER:
+            printf("Gaussan dimension: %d\r\n", imageParam.GaussianDimension);
+            matrix = allocateMatrix(imageParam.GaussianDimension);
+            LoadGaussianValue(matrix, sigma, imageParam.GaussianDimension);// load the gaussian value to the matrix
+            for (int n = 0; n < imageParam.num_comp; n++)
+                input_comps[n].perform_boundary_extension();
+            for (int n = 0; n < imageParam.num_comp; n++)
+                output_comps[n].init(imageParam.height, imageParam.width, 1); // Don't need a border for output
+            temp_comps = new  my_image_comp[imageParam.num_comp];
+            for (int n = 0; n < imageParam.num_comp; n++)
+                temp_comps[n].init(imageParam.height, imageParam.width, (imageParam.GaussianDimension - 1) / 2); // Don't need a border for output
+            for (int n = 0; n < imageParam.num_comp; n++)
+                horizontal(input_comps + n, temp_comps + n, matrix, imageParam.GaussianDimension, 0);
+            for (int n = 0; n < imageParam.num_comp; n++)
+                vertical(temp_comps + n, output_comps + n, matrix, imageParam.GaussianDimension, 0);
+            state = (imageParam.gradientFlag) ? IMAGE_GRADIENT : OUTPUT_PICTURE;
+            delete[] temp_comps;
+            break;
+        case INVERT_LAPLACIAN: {
+            int D = Image_location(&overlaid, imageParam.initheight, imageParam.width*pow(imageParam.D,3), &imageParam);
+            imageParam.D = D;
+            lapalacin = allocate_laplacian(D, imageParam.origion_image_height, imageParam.width * pow(D, 3));
+            Decompoment(overlaid, lapalacin, D);
+            my_image_comp* input = lapalacin[D];
+            my_image_comp* temp_image_1 = new  my_image_comp[imageParam.num_comp];
+            for (int i = D; i >= 1; i--) {
+                Image_upsample(&input, &temp_image_1, &imageParam);
+                temp_image_2 = ImageRestore(temp_image_1, lapalacin[i - 1], &imageParam);
+                printf("lapalacin[D - 1].height:%d\r\n", lapalacin[i - 1][0].height);
+                input = temp_image_2;
+            }
+            imageParam.width = imageParam.width * pow(imageParam.D, 3);
+            /*           imageParam.initheight = lapalacin[D][0].height;
+                       imageParam.initwidth = lapalacin[D][0].width;
+                       imageParam.D = D;*/
+            state = OUTPUT_PICTURE;
+
+        }break;
+        case LAPLACIAM_PYRAMID: {
+            my_image_comp* temp_diff = new  my_image_comp[imageParam.num_comp];
+            my_image_comp* temp_diffback = new  my_image_comp[imageParam.num_comp];
+            Image_comps_init(&temp_diff, &imageParam, imageParam.height, imageParam.width, (imageParam.sincWidth - 1) / 2);
+            temp_comps = new  my_image_comp[imageParam.num_comp];
+            Image_comps_init(&temp_comps, &imageParam, imageParam.height, imageParam.width, (imageParam.sincWidth - 1) / 2);
+
+            my_image_comp* temp_comps_inital = new  my_image_comp[imageParam.num_comp];
+            Image_comps_init(&temp_comps_inital, &imageParam, imageParam.height, imageParam.width, (imageParam.sincWidth - 1) / 2);
+
+            my_image_comp* temp_comps_3 = new  my_image_comp[imageParam.num_comp];
+            Image_comps_init(&temp_comps_3, &imageParam, imageParam.height, imageParam.width, (imageParam.sincWidth - 1) / 2);
+
+            my_image_comp* temp_comps_4 = new  my_image_comp[imageParam.num_comp];
+            Image_comps_init(&temp_comps_4, &imageParam, imageParam.height, imageParam.width, (imageParam.sincWidth - 1) / 2);
+            overlaid = new  my_image_comp[imageParam.num_comp];
+
+            tempheight = imageParam.initheight;//extension coloum
+            for (int i = 0; i < imageParam.D; i++) {
+                imageParam.initheight += (tempheight / 2);
+                tempheight = (tempheight / 2);
+            }
+            printf("initheight:%d\r\n", imageParam.initheight);
+            Image_comps_init(&overlaid, &imageParam, imageParam.initheight, imageParam.width, (imageParam.sincWidth - 1) / 2);
+            Image_LPF(&input_comps, &temp_comps_inital, &sinc, &imageParam);//lpf 10
+            Image_DownSample(&temp_comps_inital, &temp_comps, &imageParam);// 5
+            Image_upsample(&temp_comps, &temp_comps_3, &imageParam);//10
+            Laplacian_difference(&temp_comps_inital, &temp_comps_3, &imageParam);
+            Image_copy(&temp_comps_3, &overlaid, &imageParam);
+            //Image_DownSample(&temp_comps, &output_comps, &imageParam);//5
+
+            nextinput = temp_comps;
+
+            for (int i = 0; i < imageParam.D - 1; i++) {
+                Image_LPF(&nextinput, &temp_comps_4, &sinc, &imageParam);//10
+                Image_DownSample(&temp_comps_4, &output_comps, &imageParam);//5
+                Image_upsample(&output_comps, &temp_diffback, &imageParam);//10
+                Laplacian_difference(&temp_comps_4, &temp_diffback, &imageParam);//10
+                Image_copy(&temp_diffback, &overlaid, &imageParam);
+                nextinput = output_comps;
+                /* Image_copy(&output_comps, &overlaid, &imageParam);*/
+            }
+            Image_copy(&output_comps, &overlaid, &imageParam);
+
+            state = INVERT_LAPLACIAN;
+        }
+                              break;
+        case GAUSSIAN_PYRAMID:
+            temp_comps = new  my_image_comp[imageParam.num_comp];
+            overlaid = new  my_image_comp[imageParam.num_comp];
+            for (int n = 0; n < imageParam.num_comp; n++)
+                temp_comps[n].init(imageParam.height, imageParam.width, (imageParam.sincWidth - 1) / 2); // Don't need a border for output
+            for (int n = 0; n < imageParam.num_comp; n++)
+                temp_comps[n].perform_boundary_extension();
+            //*************************************************
+            tempheight = imageParam.initheight;//extension coloum
+            for (int i = 0; i < imageParam.D; i++) {
+                imageParam.initheight += (tempheight / 2);
+                tempheight = (tempheight / 2);
+            }
+            printf("initheight:%d\r\n", imageParam.initheight);
+            //*************************************************
+            for (int n = 0; n < imageParam.num_comp; n++)
+                overlaid[n].init(imageParam.initheight, imageParam.width, (imageParam.sincWidth - 1) / 2); // Don't need a border for output
+            for (int n = 0; n < imageParam.num_comp; n++)
+                overlaid[n].perform_boundary_extension();
+
+            Image_LPF(&input_comps, &temp_comps, &sinc, &imageParam);
+            Image_copy(&temp_comps, &overlaid, &imageParam);
+            Image_DownSample(&temp_comps, &output_comps, &imageParam);
+            nextinput = output_comps;
+            Image_copy(&output_comps, &overlaid, &imageParam);
+            for (int i = 0; i < imageParam.D - 1; i++) {
+                Image_LPF(&nextinput, &temp_comps, &sinc, &imageParam);
+                Image_DownSample(&temp_comps, &output_comps, &imageParam);
+                nextinput = output_comps;
+                Image_copy(&output_comps, &overlaid, &imageParam);
+            }
+            /* imageParam.width = imageParam.initwidth;
+             imageParam.height = imageParam.initheight;*/
+             // 48 24 12 6 3
+            state = OUTPUT_PICTURE;
+            delete[] temp_comps;
+            break;
+        case MOVING_AVERAGE_CHECK:
+            VarianceLoopCheck(sigma, &imageParam);
+            state = LOAD_PICTURE;
+            break;
+        case MOVING_AVERAGE:
+            matrix = allocateMatrix(imageParam.MV_Dimension);
+            MovingAverageSetValue(matrix, imageParam.MV_Dimension);
+            for (int n = 0; n < imageParam.num_comp; n++)
+                input_comps[n].perform_boundary_extension();
+            for (int n = 0; n < imageParam.num_comp; n++)
+                output_comps[n].init(imageParam.height, imageParam.width, 1); // Don't need a border for output
+            temp_comps = new  my_image_comp[imageParam.num_comp];
+            for (int n = 0; n < imageParam.num_comp; n++)
+                temp_comps[n].init(imageParam.height, imageParam.width, (imageParam.MV_Dimension - 1) / 2); // Don't need a border for output
+            for (int n = 0; n < imageParam.num_comp; n++)
+                horizontal(input_comps + n, temp_comps + n, matrix, imageParam.MV_Dimension, 1);
+            for (int n = 0; n < imageParam.num_comp; n++)
+                vertical(temp_comps + n, output_comps + n, matrix, imageParam.MV_Dimension, 1);
+            state = (imageParam.gradientFlag) ? IMAGE_GRADIENT : OUTPUT_PICTURE;
+            delete[] temp_comps;
+            break;
+        case IMAGE_GRADIENT:
+            printf("Gradient\r\n");
+#if LAPLACIAN == 1
+            //for (int i = 0; i < 3; i++) {
+            //    for (int j = 0; j < 3; j++) {
+            //        lapkernel[i][j] = laplacianKernel[i][j];
+            //    }
+            //}
+            //temp_comps = new  my_image_comp[imageParam.num_comp];
+            //for (int n = 0; n < imageParam.num_comp; n++)
+            //    temp_comps[n].init(imageParam.height, imageParam.width, 1); // Don't need a border for output
+            //for (int n = 0; n < imageParam.num_comp; n++)
+            //    apply_filter_modified(output_comps+n, temp_comps +n, lapkernel,3);
+
+            temp_comps = new  my_image_comp[imageParam.num_comp];
+            for (int n = 0; n < imageParam.num_comp; n++)
+                temp_comps[n].init(imageParam.height, imageParam.width, 1); // Don't need a border for output
+            /************************************************************************************/
+            for (int n = 0; n < imageParam.num_comp; n++)
+                temp_comps[n].SecondGrradientHorizontalFilter(output_comps + n, 3, &imageParam, imageParam.alpha);
+            for (int n = 0; n < imageParam.num_comp; n++)
+                output_comps[n].SecondGrradientverticalFilter(temp_comps + n, 3, &imageParam, imageParam.alpha);
+            /************************************************************************************/
+#else 
+            temp_comps = new  my_image_comp[imageParam.num_comp];
+            for (int n = 0; n < imageParam.num_comp; n++)
+                temp_comps[n].init(imageParam.height, imageParam.width, 1); // Don't need a border for output
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < (temp_comps[i].stride * (temp_comps[i].height + 2 * temp_comps[i].border)); j++) {
+                    temp_comps[i].handle[j] = output_comps[i].handle[j];
+                }
+            }
+            for (int n = 0; n < imageParam.num_comp; n++)
+                output_comps[n].GradientFilter(temp_comps + n, 3, imageParam.alpha);
+#endif
+            state = OUTPUT_PICTURE;
+            break;
+        case OUTPUT_PICTURE:
+#if LAPLACIAN == 1
+            //OutputImage(&out, input_comps, &temp_comps, &line, &imageParam, argv);
+            OutputImage(&out, input_comps, &output_comps, &line, &imageParam, argv);
+            bmp_in__close(&in);
+            delete[] line;
+            delete[] input_comps;
+            delete[] temp_comps;
+
+#else
+            delete line;
+            line = new io_byte[imageParam.width * 3];//* num_comps
+            OutputImage(&out, input_comps, &temp_image_2, &line, &imageParam, argv);
+            bmp_in__close(&in);
+
+            delete[] line;
+            delete[] input_comps;
+            delete[] output_comps;
+#endif
+
+
+            return 1;
+        default:
+            break;
+        }
+    }
+}
---
+++ Task4/Task4p2/image_comps.h
@@ -0,0 +1,122 @@
+﻿/*****************************************************************************/
+// File: image_comps.h
+// Author: David Taubman
+// Last Revised: 13 August, 2007
+/*****************************************************************************/
+// Copyright 2007, David Taubman, The University of New South Wales (UNSW)
+/*****************************************************************************/
+
+/*****************************************************************************/
+/* STRUCT                        my_image_comp                               */
+/*****************************************************************************/
+#ifndef IMAGE_COMPS_H
+#define IMAGE_COMPS_H
+#include <stdio.h>
+#include "filter.h"
+#define WIDTH 5
+#define HIGHT 5
+#define PI 3.14159265
+class Filter;
+//#define DEBUG
+#define CLAMP_TO_BYTE(sum) ((sum) = (sum) > 255 ? 255 : ((sum) < 0 ? 0 : (sum)))
+#define LAPLACIAN 0
+typedef enum {
+    CHECK_INPUT,
+    LOAD_PICTURE,
+    GAUSSIAN_FILTER,
+    MOVING_AVERAGE,
+    MOVING_AVERAGE_CHECK,
+    OUTPUT_PICTURE,
+    IMAGE_GRADIENT,
+    LAPLACIAN_IMAGE,
+    GAUSSIAN_PYRAMID,
+    LAPLACIAM_PYRAMID,
+    INVERT_LAPLACIAN,
+}STATE;
+typedef struct {
+    int width;
+    int height;
+    int num_comp;
+    io_byte* line;
+    int MV_Dimension;
+    int GaussianDimension;
+    bool gradientFlag;
+    float alpha;
+    int sincWidth;
+    int D;
+    int H;
+    int initwidth;
+    int initheight;
+    int origion_image_height;
+}ImageParam;
+#define GAUSSIAN 1
+#define MOVINGAVERAGE 0
+struct my_image_comp {
+    // Data members: (these occupy space in the structure's block of memory)
+    int width;
+    int height;
+    int stride;
+    int border; // Extra rows/cols to leave around the boundary
+    float* handle; // Points to start of allocated memory buffer
+    float* buf; // Points to the first real image sample
+    int height_offset;
+    // Function members: (these do not occupy any space in memory)
+    my_image_comp()
+    {
+        width = height = stride = border = 0;  handle = buf = NULL;
+        height_offset = 0;
+    }
+    ~my_image_comp()
+    {
+        if (handle != NULL) delete[] handle;
+    }
+    void init(int height, int width, int border)
+    {
+        this->width = width;  this->height = height;  this->border = border;
+        stride = width + 2 * border;//扩展后的长宽
+        if (handle != NULL)
+            delete[] handle; // Delete mem allocated by any previous `init' call
+        handle = new float[stride * (height + 2 * border)];
+        buf = handle + (border * stride) + border;//the real picture start point
+    }
+    void perform_boundary_extension();
+    void apply_filter_modified_simo(my_image_comp* in, my_image_comp* out, float** inputfilter, int width);
+    void vector_filter(my_image_comp* in, int dimension);
+    void vector_horizontal_filter(my_image_comp* in, int dimension);
+    void GrradientHorizontalFilter(my_image_comp* in, int dimension, int alpha);
+    void GrradientverticalFilter(my_image_comp* in, int width, int alpha);
+    void GradientFilter(my_image_comp* in, int width, int alpha);
+    void SecondGrradientHorizontalFilter(my_image_comp* in, int dimension, ImageParam* imagP, int alpha);
+    void SecondGrradientverticalFilter(my_image_comp* in, int dimension, ImageParam* imagP, int alpha);
+    // This function is implemented in "filtering_main.cpp".
+};
+void Image_LPF(my_image_comp** input_comps, my_image_comp** output_comps, Filter** sinc, ImageParam* imageParam);
+void apply_filter(my_image_comp* in, my_image_comp* out);
+float FilterNormalized(float** input, int dimension);
+void apply_filter_modified(my_image_comp* in, my_image_comp* out, float** inputfilter, int width);
+void apply_filter_modified_simo(my_image_comp* in, my_image_comp* out, float** inputfilter, int width);
+void unsharp_mask_filter(float** inputfilter, int width, float alpha);
+void CheckInput(int argc, char* argv[], float* sigma, int* filterChooseFlag, ImageParam* param);
+int OutputImage(bmp_out* out, my_image_comp* input_comps, my_image_comp** output_comps, io_byte** line, ImageParam* imageParam, char** argv);
+float GaussianFillKernel(int x, int y, float sigma);
+int LoadGaussianValue(float** matrix, float sigma, int dimension);
+float** allocateMatrix(int dimension);
+void FreeMatrix(float** matrix, int width);
+int VarianceLoopCheck(float sigma, ImageParam* imageParam);
+void LoadImage(bmp_in* in, my_image_comp** input_comps, my_image_comp** output_comps, io_byte** line,
+    ImageParam* imageParam, int* filterChoose, char** argv);
+void MovingAverageSetValue(float** matrix, int dimension);
+int GaussianWindowDimensionChoose(float sigma);
+void listshift(float* buffer, float** ptr, int width);
+void horizontal(my_image_comp* in, my_image_comp* out, float** inputfilter, int width, int G_MF_flag);
+void vertical(my_image_comp* in, my_image_comp* out, float** inputfilter, int width, int G_MF_flag);
+void Image_DownSample(my_image_comp** input_comps, my_image_comp** output_comps, ImageParam* imageParam);
+void Image_copy(my_image_comp** input_comps, my_image_comp** output_comps, ImageParam* imageParam);
+void Image_upsample(my_image_comp** input_comps, my_image_comp** output_comps, ImageParam* imageParam);
+void Laplacian_difference(my_image_comp** input_comps, my_image_comp** output_comps, ImageParam* imageParam);
+void Image_comps_init(my_image_comp** temp_comps, ImageParam* imageParam, int height, int width, int extention);
+int Image_location(my_image_comp** temp_comps, int Height, int width, ImageParam* imageParam);
+void Decompoment(my_image_comp* in, my_image_comp** out, int D);
+my_image_comp** allocate_laplacian(int D, int height, int width);
+my_image_comp* ImageRestore(my_image_comp* image_upsample, my_image_comp* image_laplacian, ImageParam* imageParam);
+#endif
